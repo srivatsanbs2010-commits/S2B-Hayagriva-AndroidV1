a38b9cbbb066d06448bcea9731a58f0e361e449d
srivatsanbs2010-commits <srivatsanbs2010@gmail.com>
2026-08-09 20:09:05 +0530

    Update and rename build.gradle to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
-    id 'org.jetbrains.kotlin.plugin.compose'
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
 
 android {
